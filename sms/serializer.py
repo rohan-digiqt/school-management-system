ff68e89842817401e69418dbccd412158359893a
Rohan Parmar - Digiqt <104885994+rohan-digiqt@users.noreply.github.com>
2022-06-28 19:17:42 +0530

    Version 1.1 (28/6)

--- sms/serializer.py
+++ sms/serializer.py
@@ -1,24 +1,92 @@
-from dataclasses import field
+from dataclasses import field, fields
 from pyexpat import model
 from rest_framework import serializers
 # from yaml import serializers
 
 from .models import *
 
-class DepartmentSerializer(serializers.ModelSerializer):
+#  fields = ['department_name'] #which fields you have to serialize
 
+
+class ParentSerializer(serializers.ModelSerializer):
+    
     class Meta:
-        model = Department #which model you have to serialize
-        fields = '__all__' #which fields you have to serialize
+        model = Parent
+        fields = '__all__'
 
-class DepartmentChoiceSerializer(serializers.ModelSerializer):
-
+class TeacherSerializer(serializers.ModelSerializer):
+    
     class Meta:
-        model = Department #which model you have to serialize
-        fields = ['department_name'] #which fields you have to serialize
+        model = Teacher
+        fields = '__all__'
 
+class ClassroomSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Classroom
+        fields = '__all__'
+
+class GradeSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Grade
+        fields = '__all__'
 class StudentSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Student
+        fields = '__all__'
+
+class AttendanceSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model =  Attendance
+        fields = '__all__'
+
+class ClassroomStudentSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = ClassroomStudent
+        fields = '__all__'
+
+class ExamTypeSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = ExamType
+        fields = '__all__'
+
+class ChoiceSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Choice
+        fields = '__all__'
+
+class CourseSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Course
+        fields = '__all__'
+
+class ExamSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Exam
+        fields = '__all__'
+
+class SubjectSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Subject
+        fields = '__all__'
+
+class ExamResultSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = ExamResult
+        fields = '__all__'
+
+class AdminSectionSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = AdminSection
         fields = '__all__'
