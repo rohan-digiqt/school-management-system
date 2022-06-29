1aae6467640053330326348bf0f87e59997287c5
Rohan <rohan.parmar@digiqt.com>
2022-06-29 18:12:12 +0530

    Version 1.3

---
+++ sms/constant.py
@@ -0,0 +1,15 @@
+CHOICE_1 = (
+    ('Computer','Computer'),
+    ('It','It'),
+    ('Mechanical','Mechanical'),
+    ('Civil','Civil'),
+    ('Electrical','Electrical')
+)
+
+CHOICE_2 = (
+    ('Physics','Physics'),
+    ('Chemistray','Chemistray'),
+    ('Biology','Biology'),
+    ('Maths','Maths'),
+    ('Microbiology','Microbiology')
+)
